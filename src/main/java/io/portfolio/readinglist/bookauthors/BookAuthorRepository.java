b61b531fce8eaf542bb66c4d8c3be3d9bb5b7ab6
Aish CJ <aishwarya.cj11@gmail.com>
2022-08-24 17:10:55 -0400

    Deleted unused bookAuthorId package and its files.

--- src/main/java/io/portfolio/readinglist/bookauthors/BookAuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package io.portfolio.readinglist.bookauthors;
-
-
-import io.portfolio.readinglist.models.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookAuthorRepository extends JpaRepository<Author,Long> {
-}
